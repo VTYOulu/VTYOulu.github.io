4cce419562359632a6f5a31648d69e8e65287eb8
Ville-Veikka Kiiskilä <villeveikkak@gmail.com>
2024-04-18 17:16:53 +0300

    Bottombar clipping fixed

--- VTYwebsite/src/components/BottomBar.jsx
+++ VTYwebsite/src/components/BottomBar.jsx
@@ -1,7 +1,7 @@
 const BottomBar = (props) => {
     const currentYear = new Date().getFullYear();
     return (
-      <footer className="font-sans bg-neutral-800 py-12 px-6 flex items-center justify-center h-lg">
+      <footer className="font-sans bg-neutral-800 py-12 px-6 flex items-center justify-center h-lg mt-16">
         <div className="flex items-center lg:mr-80">
           <h3 className="text-gray-400 text-sm ml-2 p-2 font-semibold">© Oulun yliopiston Vesiteknillinen Yhdistys ry {currentYear}</h3>
           <h3 className="text-gray-400 text-sm font-semibold"> - </h3>
