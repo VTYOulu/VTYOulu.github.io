e4c71079b1e6ebe5eee769a0875dced2e89b4d48
Ville-Veikka Kiiskilä <97534406+V-vTK@users.noreply.github.com>
2024-08-29 18:07:38 +0300

    Update BottomBar.jsx

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -2,8 +2,8 @@ const BottomBar = (props) => {
     const currentYear = new Date().getFullYear();
     return (
       <footer className="font-sans bg-neutral-800 py-12 px-6 flex items-center justify-center h-lg mt-16">
-        <div className="flex items-center lg:mr-80">
-          <h3 className="text-gray-400 text-sm ml-2 p-2 font-semibold">© Oulun yliopiston Vesiteknillinen Yhdistys ry {currentYear}</h3>
+        <div className="flex items-center">
+          <h3 className="text-gray-400 text-sm p-2 font-semibold">© Oulun yliopiston Vesiteknillinen Yhdistys ry {currentYear}</h3>
           <h3 className="text-gray-400 text-sm font-semibold"> - </h3>
           <button className="text-gray-400 text-sm p-2 font-semibold hover:brightness-110" href={"Tietosuojaseloste"} onClick={() => props.onItemClick("Tietosuojaseloste")}> Tietosuojaseloste</button>
         </div>
