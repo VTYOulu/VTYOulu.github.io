d91a3ce4a1f04821c4e779c95bf9a26e71b26eb9
VTYOulu <167357775+VTYOulu@users.noreply.github.com>
2024-09-11 22:15:18 +0300

    Update OutsideExcursion.jsx

--- src/components/pages/OutsideExcursion.jsx
+++ src/components/pages/OutsideExcursion.jsx
@@ -5,7 +5,11 @@ const OutsideExcursion = ({onItemClick}) => {
   return ( 
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Ulkomaan excursio</h1>
-      <p className="text-xl text-gray-700"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, voluptatem officia suscipit dignissimos deleniti illum quis. In vitae nam ratione! A cupiditate magni rem enim aut corrupti voluptates mollitia? Velit.</p>
+      <p className="text-xl text-gray-700">
+        VTY:n odotetuimpiin tapahtumiin lukeutuu joka toinen vuosi järjestettävä ulkomaan excursio. Tämä matka tarjoaa jäsenille ainutlaatuisen tilaisuuden tutustua kansainväliseen työelämään, kulttuuriin ja verkostoitumismahdollisuuksiin rennossa ja innostavassa ilmapiirissä.<br></br>
+        Excursio suuntautuu joka kerta eri maahan, jossa osallistujat pääsevät vierailemaan paikallisissa yrityksissä ja organisaatioissa. Vierailujen aikana tutustutaan paikallisiin yliopistoihin, sekä muihin alaan liittyviin vierailukohteisiin.<br></br>
+        Matkan ohjelmaan kuuluu myös vapaa-ajan aktiviteetteja, kuten kaupunkikierroksia, kulttuurikohteisiin tutustumista ja yhteisiä illanviettoja.
+      </p>
     </div>
   );
 };
