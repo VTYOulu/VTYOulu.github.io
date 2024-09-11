0ebe41f8e213c6b224024fe460307ba398831ba7
VTYOulu <167357775+VTYOulu@users.noreply.github.com>
2024-09-11 22:20:06 +0300

    Update OutsideExcursion.jsx

--- src/components/pages/OutsideExcursion.jsx
+++ src/components/pages/OutsideExcursion.jsx
@@ -6,8 +6,12 @@ const OutsideExcursion = ({onItemClick}) => {
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Ulkomaan excursio</h1>
       <p className="text-xl text-gray-700">
-        VTY:n odotetuimpiin tapahtumiin lukeutuu joka toinen vuosi järjestettävä ulkomaan excursio. Tämä matka tarjoaa jäsenille ainutlaatuisen tilaisuuden tutustua kansainväliseen työelämään, kulttuuriin ja verkostoitumismahdollisuuksiin rennossa ja innostavassa ilmapiirissä.<br></br>
-        Excursio suuntautuu joka kerta eri maahan, jossa osallistujat pääsevät vierailemaan paikallisissa yrityksissä ja organisaatioissa. Vierailujen aikana tutustutaan paikallisiin yliopistoihin, sekä muihin alaan liittyviin vierailukohteisiin.<br></br>
+        VTY:n odotetuimpiin tapahtumiin lukeutuu joka toinen vuosi järjestettävä ulkomaan excursio. Tämä matka tarjoaa jäsenille ainutlaatuisen tilaisuuden tutustua kansainväliseen työelämään, kulttuuriin ja verkostoitumismahdollisuuksiin rennossa ja innostavassa ilmapiirissä.
+      </p>
+      <p className="text-xl text-gray-700 mt-2">
+        Excursio suuntautuu joka kerta eri maahan, jossa osallistujat pääsevät vierailemaan paikallisissa yrityksissä ja organisaatioissa. Vierailujen aikana tutustutaan paikallisiin yliopistoihin, sekä muihin alaan liittyviin vierailukohteisiin.
+      </p>
+      <p className="text-xl text-gray-700 mt-2">
         Matkan ohjelmaan kuuluu myös vapaa-ajan aktiviteetteja, kuten kaupunkikierroksia, kulttuurikohteisiin tutustumista ja yhteisiä illanviettoja.
       </p>
     </div>
