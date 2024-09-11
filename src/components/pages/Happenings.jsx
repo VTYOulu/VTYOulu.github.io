c27fdcb0bc68b15e8edc0eb43c8eaa7c4237b68d
VTYOulu <167357775+VTYOulu@users.noreply.github.com>
2024-09-11 22:57:13 +0300

    Update Happenings.jsx

--- src/components/pages/Happenings.jsx
+++ src/components/pages/Happenings.jsx
@@ -5,7 +5,10 @@ const Happenings = ({onItemClick}) => {
   return ( 
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Tapahtumat</h1>
-      <p className="text-xl text-gray-700"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, voluptatem officia suscipit dignissimos deleniti illum quis. In vitae nam ratione! A cupiditate magni rem enim aut corrupti voluptates mollitia? Velit.</p>
+      <p className="text-xl text-gray-700"> 
+        VTY:n pääasiallinen toiminta perustuu tapahtumien järjestämiseen. Järjestämme jäsenillemme monenlaisia tapahtumia vesitekniikkaan liittyen. Tapahtumiamme ovat muun muassa aamupalaseminaarit, iltapäiväkerhot, excursiot kotimaahan tai ulkomaille, polkupyöräexcursiot sekä tietysti pikkujoulut.
+        VTY:n tapahtumiin voi liittyä matalalla kynnyksellä. 
+      </p>
     </div>
   );
 };
