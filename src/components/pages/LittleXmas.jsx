3c17da7419f6c94d69d844fbcacb7bb3a1024697
VTYOulu <167357775+VTYOulu@users.noreply.github.com>
2024-09-11 21:50:04 +0300

    Update LittleXmas.jsx

--- src/components/pages/LittleXmas.jsx
+++ src/components/pages/LittleXmas.jsx
@@ -5,7 +5,7 @@ const LittleXmas = ({onItemClick}) => {
   return ( 
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Pikkujoulut</h1>
-      <p className="text-xl text-gray-700"> Pikkujoulut ovat ehkä perinteisikkäin... 1§ 213 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, voluptatem officia suscipit dignissimos deleniti illum quis. In vitae nam ratione! A cupiditate magni rem enim aut corrupti voluptates mollitia? Velit.</p>
+      <p className="text-xl text-gray-700"> Pikkujoulut ovat yhdistyksemme vuosittainen perinne, jossa jäsenet kokoontuvat viettämään rentoa ja yhteisöllistä iltaa joulun tunnelmissa. Tapahtuma alkaa yhdistyksen vaalikokouksella, jossa käydään läpi ajankohtaisia asioita ja valitaan uusi hallitus seuraavalle kaudelle. Kokouksen jälkeen ilta jatkuu pikkujoulutunnelmissa: tarjolla on perinteisiä jouluruokia, mahdollisuus saunoa ja rentoutua paljussa, sekä monipuolista ohjelmaa, kuten esityksiä, leikkimielisiä kilpailuja ja musiikkia, joka yhdistää joulun ja vesitekniikan teemat. Pikkujoulut tarjoavat mukavan tavan viettää aikaa yhdessä ja virittäytyä joulun odotukseen. VTY:n pikkujouluja ei siis kannata jättää välistä!</p>
     </div>
   );
 };
