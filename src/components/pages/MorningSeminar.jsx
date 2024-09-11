909c11ab79dbc8a1ec9229af2f359e9bf86580a3
VTYOulu <167357775+VTYOulu@users.noreply.github.com>
2024-09-11 22:35:37 +0300

    Update MorningSeminar.jsx

--- src/components/pages/MorningSeminar.jsx
+++ src/components/pages/MorningSeminar.jsx
@@ -5,7 +5,9 @@ const MorningSeminar = ({onItemClick}) => {
   return ( 
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Aamupalaseminaari</h1>
-      <p className="text-xl text-gray-700"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, voluptatem officia suscipit dignissimos deleniti illum quis. In vitae nam ratione! A cupiditate magni rem enim aut corrupti voluptates mollitia? Velit.</p>
+      <p className="text-xl text-gray-700">
+        VTY:n aamupalaseminaari tarjoaa jäsenille mahdollisuuden aloittaa päivä inspiroivassa ilmapiirissä, hyvän seuran ja herkullisen aamupalan parissa. Aamupalaseminaarin ohjelmassa on yritysten tietoiskuja ja yritysesittelyitä, joissa kuullaan ajankohtaisista aiheista, yritysten projekteista sekä työmahdollisuuksista alan parissa. Aamupalaseminaareissa tarjotaan myös maukas aamupala, joka tarjoaa energiaa päivän aloitukseen. 
+      </p>
     </div>
   );
 };
