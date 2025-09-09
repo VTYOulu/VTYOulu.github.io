367d4dc584963c7dbde72d4850447fb9d4998db1
Anni-Noora Niemelä <anninoora@gmail.com>
2025-09-09 20:01:45 +0300

    Updated annual report

--- src/components/pages/AnnualReport.jsx
+++ src/components/pages/AnnualReport.jsx
@@ -6,36 +6,43 @@ const AnnualReport = ({onItemClick}) => {
     <div className="max-w-3xl p-4">
       <h1 className="text-3xl font-semibold text-left mb-4">Toimintakertomus</h1>
         <div className="text-xl text-gray-700">
-          <p> Yhdistyksen toiminta pysyi aktiivisena toimikaudella 2023. Hallitus kokoontui toimikauden aikana 12 kertaa ja 
-              toimikauden aikana järjestettiin sääntömääräinen vuosikokous maaliskuussa sekä vaalikokous joulukuussa. 
-              Toimikauden aikana uusia jäseniä liittyi 24 kappaletta. 
+          <p> 
+            Yhdistys jatkoi toimikaudella 2024 aktiivista toimintaansa. Toimikauden vuosikokous 
+            järjestettiin maaliskuussa ja vaalikokous joulukuussa. Hallituksen kokouksia pidettiin 13 
+            näiden lisäksi. Uusia jäseniä liittyi yhteensä 20 toimikauden aikana.  
           </p>
           <br></br>
           <p>
-            Hallitus aloitti toimikauden perehtymällä uusiin tehtäviin, ryhmäytymisellä sekä hallituksenvaihtosauna-illalla yhdessä edellisen toimikauden hallituksen jäsenien kanssa. Toimikauden aikana järjestettiin keväällä 
-            polkupyöräexcursio, jonka kohteina olivat AFRY Finland Oy:n vesitekniikan osasto ja Pohjois-Pohjanmaan ELY-keskus. Syksyllä järjestettiin kotimaan excursio, jonka kohteena oli Lappi ja Rovaniemi. Rovaniemellä yhdistys 
-            vieraili Lapin ELY-keskuksella ja Nevellä. Toimikauden aikana järjestettiin myös seminaari yhteistyössä AFRYn, 
-            Sitowisen, Rambollin ja RILlin kanssa, jossa oli tarjolla osallistujille aamupalaa ja kuulimme vesitekniikan 
-            opiskelijoiden työmahdollisuuksista sekä yritysten meneillään olevista kiinnostavista projekteista. Lisäksi 
-            toimikauden aikana on järjestetty iltapäiväkerhoja, joissa on ollut tarjolla kahvia, pientä purtavaa ja 
-            mahdollisuus tutustua yhdistyksen toimintaan sekä viettää aikaa muiden Teekkareiden kanssa. Yhdistys 
-            järjesti lisäksi Finanssin PeliVeikkojen kanssa yhteisen peli-illan. Joulukuussa pidettiin Pub Kuutio -ilta, jossa 
-            yhdistys vieraili Oulun Kaijonharjussa sijaitsevassa Pub Kuutiossa viettämässä iltaa ja verkostoitumassa 
-            yhdistyksen muiden jäsenten kanssa.
+            Hallituksen toimikausi alkoi hallituksen ja toimihenkilöiden perehtymisellä tehtäviinsä ja 
+            tutustumisella toisiinsa. Tähän kuului myös hallituksenvaihtosauna, joka vietettiin yhdessä 
+            edellisen toimikauden hallituslaisten ja toimihenkilöiden kanssa. Pitkän tauon jälkeen yhdistys 
+            järjesti 40-vuotisen taipaleensa kunniaksi vuosijuhlat. Juhliin saapui yhdistyksen nykyisten 
+            jäsenten lisäksi myös alumneja ja yhteistyökumppaneita.  
           </p>
           <br></br>
           <p>
-            Yhdistyksen toimintaa on toimikauden aikana esitelty Ympäristörakentajakillan fuksi-infossa sekä 
-            Ympäristörakentajakillan pestiesittelyssä, jossa Ympäristörakentajakillan sekä Vesiteknillisen yhdistyksen 
-            hallitukset esittelivät hallitustensa toimintaa Ympäristörakentajakillan jäsenistölle. Joulukuussa järjestettiin 
-            perinteisesti vaalikokouksen yhteydessä yhdistyksen pikkujoulut Oulun kiipeilykeskuksella. Pikkujoulujen 
-            osallistujamäärä oli 50, joten yhdistyksen toiminta on tavoittanut toimikauden aikana hyvän määrän 
-            vesitekniikasta kiinnostuneita Teekkareita. Pikkujouluissa oli tarjolla jouluruokaa ja -juomaa sekä 
-            saunomismahdollisuus ja pikkujouluihin oli pyydetty vesitekniikan alan diplomityön esittely.</p>
+            Keväällä järjestettiin perinteinen polkupyöräexcursio, jossa vierailtiin Oulun Veden Taskilan 
+            jätevedenpuhdistamolla ja Suomen ympäristökeskuksen toimitiloissa. Keväällä yhdistys piti 
+            myös iltapäiväkerhon, jossa vietettiin aikaa lautapelien ja haalarimerkkien ompelemisen 
+            merkeissä. Syksyllä yhdistys järjesti ulkomaan excursion, jonka kohteena oli tällä kertaa Praha. 
+            Prahassa yhdistys vieraili kahdella vesiteknillistä alaa opettavalla yliopistolla, vesilaitoksella 
+            sekä nykyisellä ja entisellä jätevedenpuhdistamolla. Syksyn ohjelmaan kuului myös 
+            aamupalaseminaari, jossa aamupalan lisäksi mukana olivat Ramboll, AFRY, WSP ja Sitowise 
+            esittelemässä omia uramahdollisuuksiaan ja projektejaan. 
+          </p>
           <br></br>
-          <p className='mt-4 font-light'>18.1.2024 Oulussa</p>
-          <p className='mt-2 font-light'>Maria Wiik</p>
-          <p className='mt-2 font-light'>Puheenjohtaja 2023</p>
+          <p>
+            Yhdistys oli edustettuna keväällä Tampereella Flokki ry:n ja syksyllä Oulussa 
+            Ympäristörakentajakillan vuosijuhlilla. Yhdistystä esiteltiin myös Ympäristörakentajakillan 
+            fuksi-infossa sekä pestiesittelyssä. Viimeisen hallituksen kokouksen ja vaalikokouksen 
+            yhteydessä järjestettiin yhdistyksen pikkujoulut Oulun kiipeilykeskuksen saunatiloissa. 
+            Pikkujouluissa mm. saunottiin ja tarjottiin jouluruokia- ja juomia. Ohjelmassa oli myös alan 
+            diplomityön ja väitöskirjan esittely sekä RIL ry:n toimintaa. 
+          </p>
+          <br></br>
+          <p className='mt-4 font-light'>22.1.2025 Oulussa</p>
+          <p className='mt-2 font-light'>Jaakko Nissilä</p>
+          <p className='mt-2 font-light'>Puheenjohtaja 2024</p>
       </div>
     </div>
   );
